d22a8c49ae2c01348f52f37e73b03d99fb284c43
Jia Li <60761935+jiali120@users.noreply.github.com>
2022-11-13 16:43:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ document.getElementById("root").appendChild(h1);
 
 
 
-/*
+/* 2. 
 practice
 //Create a react app from scratch.
 //It should display a h1 heading.
@@ -50,6 +50,54 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+*/
 
 
+/* how to add js
+* Javascript Expressions in JSX & ES6 Template Literals
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname="Jia";
+const lname="Li"
+const num=7;
+
+ReactDOM.render(
+  <div>
+     //expression VS. statement in JavaScript
+     //we've got interpreted as string by using ES6 template literals : <h1>Hello {`${fname} ${lname}`}!</h1>
+ //and then we've got that string being inserted in as JS these curly braces from JSX.
+ // and then it's being inserted as a string into our h1 and then that is being inserted as JS code into JS file
+ <h1>Hello {fname+" "+lname}!</h1> // Hello {fname} {lname} also is corret
+ 
+  <p>Your lucky number is {Math.floor(Math.random()*10)}</p>
+  </div>, 
+  document.getElementById("root")
+);
+*/
+
+/* 3. date and year practice
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+const name="Jia";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Creat By Your Name: {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+)
 */
