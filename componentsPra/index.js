d26190f5f3185817e15ad858ee7b3d5bc11f1394
Jia Li <60761935+jiali120@users.noreply.github.com>
2022-11-16 17:17:49 -0500

    Create index.js

---
+++ componentsPra/index.js
@@ -0,0 +1,6 @@
+// index.js is not in compinentsPra file
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
