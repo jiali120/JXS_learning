c00dc645e29868f16046cd00ca3d80bdaf1b5d14
Jia Li <60761935+jiali120@users.noreply.github.com>
2022-11-17 18:32:57 -0500

    Create Footer.jsx

---
+++ keeper-app-part-1-starting/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+
+    const currentYear=new Date().getFullYear();
+    return <p>
+        Copyright © {currentYear}
+    </p>
+}
+
+export default Footer;
